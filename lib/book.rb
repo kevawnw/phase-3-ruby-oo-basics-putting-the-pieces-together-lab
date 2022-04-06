959b584610eb7e7f7012cb55d8abc2d27b5324ad
Kevawn Warren <kevawnw@gmail.com>
2022-04-06 13:55:31 -0400

    commit tree 42b32a5681618ccba1573907f7e665e18a660fb0

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+    attr_reader :title 
+    attr_accessor :author, :genre
+    
+    def initialize(title)
+        @title = title
+    end
 
+    def page_count=(number)
+        @number = number
+    end
+    
+    def page_count
+        @number
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+
+    # def author=(name)
+    #     @name = name
+    # end
+
+    # def author
+    #     @name
+    # end
+
+    
+
+
+    
+
+
+
+end
 
